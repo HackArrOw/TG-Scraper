fb4d42433f261895d04d597f4503fb634947405f
HackArrOw <94838895+HackArrOw@users.noreply.github.com>
2021-11-23 13:01:28 +0000

    Update adder.py

--- adder.py
+++ adder.py
@@ -1,7 +1,4 @@
 #!/bin/env python3
-# (c) @AbirHasan2005
-# Telegram Group: http://t.me/linux_repo
-# Please give me credits if you use any codes from here.
 
 
 print ("\033[1;92m")
@@ -9,7 +6,7 @@ print ("░█▀█░█▀▄░█▀▄░█▀▀░█▀▄")
 print ("░█▀█░█░█░█░█░█▀▀░█▀▄")
 print ("░▀░▀░▀▀░░▀▀░░▀▀▀░▀░▀")
 print ("")
-print ("      by \033[1;95m@AbirHasan2005")
+print ("      by \033[1;95m@ HackerArrOw")
 print ("\033[1;92m")
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
